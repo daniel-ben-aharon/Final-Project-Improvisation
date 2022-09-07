83ce28ad925ba02ab919d583f419014f6c3c4a41
daniel-ben-aharon <58771165+daniel-ben-aharon@users.noreply.github.com>
2022-09-07 16:27:57 +0300

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -17,7 +17,3 @@ my_cursor.execute("CREATE DATABASE userdb")
 #    print(db)
 my_cursor.execute("CREATE TABLE acounts (name VARCHAR(255),email VARCHAR(255), password VARCHAR(255), user_id cINTEGER AUTO_INCREMENT PRIMARY Key"))
 my_cursor.execute("SHOW TABLES")
-#for table in my_cursor:
-#    print(table)     # table[0] to get another formatt (without parantesis)
-#sqlStuff = "INSERT INFO users (name, email, age) VALUES (%s, %s, %s)"
-#record1 = ("John" , "john@codemy.com", 40)
