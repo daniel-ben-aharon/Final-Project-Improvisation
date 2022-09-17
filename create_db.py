01677a2e1ec105388ae3d01857b1be8c26152585
daniel-ben-aharon <58771165+daniel-ben-aharon@users.noreply.github.com>
2022-09-17 21:35:08 +0300

    removing unnecessary lines

--- create_db.py
+++ create_db.py
@@ -16,6 +16,3 @@ my_cursor.execute("CREATE DATABASE userdb")
 # my_cursor.execute("SHOW DATABASES")
 # for db in my_cursor:
 #    print(db)
-
-my_cursor.execute("CREATE TABLE acounts (name VARCHAR(255),email VARCHAR(255), password VARCHAR(255), user_id cINTEGER AUTO_INCREMENT PRIMARY Key")
-my_cursor.execute("SHOW TABLES")
