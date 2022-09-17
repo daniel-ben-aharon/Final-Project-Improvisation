e8c2495558b6be84ab5c1ad3da9e911934e8491c
ShakedSpec <71656084+ShakedSpec@users.noreply.github.com>
2022-09-17 20:55:25 +0300

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -4,7 +4,8 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="danielmysql123benaharondb#&12*-a",
+    password="Fu!!@cc3$",
+    #passwd="danielmysql123benaharondb#&12*-a",
     database="userdb"
 )
 
@@ -16,5 +17,5 @@ my_cursor.execute("CREATE DATABASE userdb")
 # for db in my_cursor:
 #    print(db)
 
-my_cursor.execute("CREATE TABLE acounts (name VARCHAR(255),email VARCHAR(255), password VARCHAR(255), user_id cINTEGER AUTO_INCREMENT PRIMARY Key"))
+my_cursor.execute("CREATE TABLE acounts (name VARCHAR(255),email VARCHAR(255), password VARCHAR(255), user_id cINTEGER AUTO_INCREMENT PRIMARY Key")
 my_cursor.execute("SHOW TABLES")
