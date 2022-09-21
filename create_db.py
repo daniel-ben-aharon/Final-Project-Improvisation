3bf77e74bad78d8f9a3e9d7fb87035ee09c3cd6e
ShakedSpec <71656084+ShakedSpec@users.noreply.github.com>
2022-09-21 14:13:24 +0300

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -6,12 +6,12 @@ mydb = mysql.connector.connect(
     user="root",
     password="Fu!!@cc3$",
     #passwd="danielmysql123benaharondb#&12*-a",
-    database="userdb"
+    #database="userdb"
 )
 
 my_cursor = mydb.cursor()
 # create database in mysql
-my_cursor.execute("CREATE DATABASE userdb")
+my_cursor.execute("CREATE DATABASE shakeddb")
 
 # my_cursor.execute("SHOW DATABASES")
 # for db in my_cursor:
