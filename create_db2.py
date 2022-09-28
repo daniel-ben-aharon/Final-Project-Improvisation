edf24a2db1cb328811e20e06f614f58fa4a98e36
ShakedSpec <71656084+ShakedSpec@users.noreply.github.com>
2022-09-28 06:38:30 +0300

    Update create_db2.py

--- create_db2.py
+++ create_db2.py
@@ -6,33 +6,38 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="Fu!!@cc3$",
+    password="123",
     #passwd="danielmysql123benaharondb#&12*-a",
     database="userdb"
 )
 
 cur = mydb.cursor()
 
-CREATE_TABLE = '''CREATE TABLE `userdb`.`XMLTable1` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `XML` MEDIUMTEXT NULL,
-  PRIMARY KEY (`id`));'''
+# CREATE_TABLE = '''CREATE TABLE `userdb`.`XMLTable2` (
+#   `id` INT NOT NULL AUTO_INCREMENT,
+#   `name` VARCHAR(500),
+#   `XML` MEDIUMTEXT NULL,
+#   PRIMARY KEY (`id`));'''
 
-cur.execute(CREATE_TABLE)
+# cur.execute(CREATE_TABLE)
+# mydb.commit()
 
 xmlData = None
 
 path_of_the_directory = 'C:\charlie_parker'  # full directory path of musicXML files
-for filename in os.listdir(path_of_the_directory):
+items_in_folder = os.listdir(path_of_the_directory)
+#print(items_in_folder)
+for filename in items_in_folder:
     f = os.path.join(path_of_the_directory, filename)
     if os.path.isfile(f):
         if f.endswith('.xml'):
             with open(f, 'r') as f:
                 xmlData = f.read()
 
-            INSERT_QUERY = """INSERT INTO XMLTable1 (XML) VALUES (%s)"""
-
-            cur.execute(INSERT_QUERY, (xmlData,))
-            mydb.commit()
+            INSERT_QUERY = f"INSERT INTO XMLTable2 (XML, name) VALUES (%s, %s)"
+            values = (xmlData,filename)
+            cur.execute(INSERT_QUERY,values)
+          
+            mydb.commit()   
             #cur.execute('select * from XMLTable')
             #results = cur.fetchall()
