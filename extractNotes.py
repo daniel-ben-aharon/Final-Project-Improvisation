f9a82f8b4bbc3d63f4ab05d0d9b9033bddf71af9
daniel-ben-aharon <58771165+daniel-ben-aharon@users.noreply.github.com>
2022-10-03 12:31:54 +0300

    Improvise function and example

--- extractNotes.py
+++ extractNotes.py
@@ -1,6 +1,7 @@
 from music21 import *
 import random
 
+
 def createDict(fileName):
     """Gets musicXML file name and create a dictionary from it
        here the key is the Chord and the value is sequence of notes play while chord plays
@@ -19,6 +20,17 @@ def createDict(fileName):
     chordOrder = []
     dict = {}
     myScore = converter.parse(fileName)
+    #tempo_f = myScore.getElementsByClass('Stream.stream')
+
+    tempo_f = myScore.getElementsByClass('tempo.MetronomeMark')
+    #
+    # print(myScore.getElementsByClass('tempo.MetronomeMart').__getattribute__())
+    # tempo.MetronomeMark('slow').number
+    #r = tempo_f.getElementsByClass('MetronomeMark').number
+    #print(r)
+    #tempo_f = myScore.getElementsByClass('MetronomeMark')['number']
+    #print(tempo_f.number)
+
     for inx in range(len(myScore.recurse().notesAndRests)):
         item = myScore.recurse().notesAndRests[inx]
         seq = []
@@ -38,10 +50,7 @@ def createDict(fileName):
                 dict[figure].append(seq)
             else:
                 dict[figure] = [seq]
-    return dict,chordOrder
-
-filename = 'Anthropology.xml'      # better recieve file's name from the user to prevent error code
-dictionary, chordsByOrder = createDict(filename)
+    return dict,chordOrder,tempo_f
 
 # Convert Duration into str of number
 def durationToInt(d):
@@ -53,11 +62,13 @@ def durationToInt(d):
         return '0.125'
 
 
-#Calculate each seq duration in dict values
-keysDurations = []
-
-# for each key
-for k in list(dictionary.keys()):
+def improvise(filename):
+  #Calculate each seq duration in dict values
+  dictionary, chordsByOrder, old_tempo = createDict(filename)
+  title_music_sheet = filename.split(".")[0]
+  keysDurations = []
+  # for each key
+  for k in list(dictionary.keys()):
     chordDuration = []
     # for each sequence
     for seq in dictionary[k]:
@@ -70,93 +81,105 @@ for k in list(dictionary.keys()):
 
             # if it is a Rest
             else:
-              if durationToInt(item.duration.fullName) is not None:
-                seqDuration = seqDuration + float(durationToInt(item.duration.fullName))
+                if durationToInt(item.duration.fullName) is not None:
+                    seqDuration = seqDuration + float(durationToInt(item.duration.fullName))
         chordDuration.append(seqDuration)
 
     # create list of list duration of each sequence in each key
     keysDurations.append(chordDuration)
 
-
 ###########################################################################################
 ####################### get a chord by the user  ##########################################
-chosen_chord_Indx = 0    # by default for our improvisation algorithm
+  chosen_chord_Indx = 0  # by default for our improvisation algorithm
+
+
+
 
 #######################################################################################################
-##  lines 89-90, 104-105, 108 in comment - if we chose random sequence from all the possible options
+##  lines 91, 103-104, 107 in comment - if we chose random sequence from all the possible options
 #######################################################################################################
+  # locs = []    # if we pick a random index
+  # chosen chord
+  for i in range(len(keysDurations[chosen_chord_Indx])):
+      item = (keysDurations[chosen_chord_Indx])[i]
+      try:
+          loc = (keysDurations[chosen_chord_Indx]).index(item, i + 1)
+      except ValueError:
+          pass
+      else:
+          if loc is not None:
+              swap_l = i  # first occurence seq - left index to swap
+              swap_r = loc  # second occurence seq - right index to swap
+              # if we pick randomly index
+              # loc.append(loc)
+              break
 
-# locs = []    ## if we pick a random index
-# chosen chord
-for i in range(len(keysDurations[chosen_chord_Indx])):
-     item = (keysDurations[chosen_chord_Indx])[i]
-     try:
-         loc =  (keysDurations[chosen_chord_Indx]).index(item,i+1)
-     except ValueError:
-         pass
-     else:
-         if loc is not None:
-            swap_l = i       # first occurence seq - left index to swap
-            swap_r = loc     # second occurence seq - right index to swap
-            # if we pick randomly index
-            # loc.append(loc)
-            break
-
-# randIndx = locs[random.randint(0,locs)]  # pick one sequence randomally
-
-
-improvise_stream = stream.Stream()
-
-#####################################################################################
-#   Following code Lines Test show method only
-#####################################################################################
-
-#configure.run()  ## To use show() method - run this function once, choose No options and then put it on comment in next time
-
-chosen_chord = list(dictionary.keys())[chosen_chord_Indx]
-print(chosen_chord)
-
-i = 0    # index of seq of chosen_chord
-
-# # run over all chord of the original musicXML file by order
-for c in chordsByOrder:
-
-    # add Chord sign to the improvised music sheet
-    improvise_stream.append(c)
-
-    # gets Chord short name:  'F7', 'B-' etc.
-    chordName = c.figure
-
-    # copy the non-improvise parts the same as original
-    if chordName != chosen_chord:
-         # copy the suitable sequence
-         for item in (dictionary[chordName])[0]:
-            improvise_stream.append(item)
-
-         # delete it from original dict to copy from
-         del (dictionary[chordName])[0]
+  # randIndx = locs[random.randint(0,locs)]  # pick one sequence randomally
 
 
-     # if we reached to the improvised chord
-    else:
-        #  swap the sequence to improvise
-        if i == swap_l:
-            # copy the suitable sequence
-            for item in (dictionary[chordName])[swap_r]:
-                improvise_stream.append(item)
-            i += 1
+  improvise_stream = stream.Stream()
 
-        elif i == swap_r:
-            # copy the suitable sequence
-            for item in (dictionary[chordName])[swap_l]:
-                improvise_stream.append(item)
-            i += 1
+  # another parameter improvisation - fast
+  #improvise_stream.append(tempo.MetronomeMark(number=150))
+  #improvise_stream.append(old_tempo)
+  # update title name
+  improvise_stream.insert(0, metadata.Metadata())
+  improvise_stream.metadata.title = 'Improvised - ' + title_music_sheet
 
-        else:
-            # copy the suitable sequence
-            for item in (dictionary[chordName])[i]:
-                improvise_stream.append(item)
-            i += 1
+  improvise_stream.insert(0, metadata.Metadata())
+  improvise_stream.metadata.composer = " "  # we should change it to modulary
+# configure.run()  ## To use show() method - run this function once, choose No options and then put it on comment in next time
 
-# Show the improvised music sheet (in musescore3)
-improvise_stream.show()
+  chosen_chord = list(dictionary.keys())[chosen_chord_Indx]
+
+  indx = 0  # index of seq of chosen_chord
+  # swap_l = 0
+  # swap_r = 0
+  # run over all chord of the original musicXML file by order
+  for c in chordsByOrder:
+      # add Chord sign to the improvised music sheet
+      improvise_stream.append(c)
+
+      # gets Chord short name:  'F7', 'B-' etc.
+      chordName = c.figure
+
+      # copy the non-improvise parts the same as original
+      if chordName != chosen_chord:
+          # copy the suitable sequence
+          for item in (dictionary[chordName])[0]:
+              improvise_stream.append(item)
+
+          # delete it from original dict to copy from
+          del (dictionary[chordName])[0]
+
+
+      # if we reached to the improvised chord
+      else:
+          #  swap the sequence to improvise
+          if indx == swap_l:
+              # copy the suitable sequence
+              for item in (dictionary[chordName])[swap_r]:
+                  improvise_stream.append(item)
+              indx += 1
+
+          elif indx == swap_r:
+              # copy the suitable sequence
+              for item in (dictionary[chordName])[swap_l]:
+                  improvise_stream.append(item)
+              indx += 1
+
+          else:
+              # copy the original sequences from musicXML file
+              for item in (dictionary[chordName])[indx]:
+                  improvise_stream.append(item)
+              indx += 1
+
+  # Show the improvised music sheet (in musescore3)
+  improvise_stream.show()
+  play(improvise_stream)
+#######################################################################################################################
+##################################  Test improvise function  ##########################################################
+#######################################################################################################################
+
+filename = 'Anthropology.xml'      # better recieve file's name from the user to prevent error code
+improvise(filename)
