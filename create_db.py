af32b7653791d21aa866fb2cb6496b1e70fa91c9
daniel-ben-aharon <58771165+daniel-ben-aharon@users.noreply.github.com>
2022-10-22 23:11:49 +0300

    Script to create userdb DB and improvs & xmltable tables
    Don't forget to change your own MySQL local password

--- create_db.py
+++ create_db.py
@@ -1,18 +1,55 @@
 import mysql.connector
-
+import os
 # connection to DB
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="123",
-    #passwd="danielmysql123benaharondb#&12*-a",
-    #database="userdb"
-)
 
-my_cursor = mydb.cursor()
-# create database in mysql
-my_cursor.execute("CREATE DATABASE shakeddb")
+def createDB():
+    firstdb = mysql.connector.connect(host="localhost",user="root", passwd="danielmysql123benaharondb#&12*-a")
+    my_cursor = firstdb.cursor()
+    my_cursor.execute("CREATE DATABASE userdb")
 
-# my_cursor.execute("SHOW DATABASES")
-# for db in my_cursor:
-#    print(db)
+
+def createTables():
+    mydb = mysql.connector.connect(host="localhost",user="root",passwd="danielmysql123benaharondb#&12*-a",database="userdb")
+
+    CREATE_TABLE1 = '''CREATE TABLE `userdb`.`improvs` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+     `name` VARCHAR(500),
+     `XML` MEDIUMTEXT NULL,
+      PRIMARY KEY (`id`));'''
+
+    cursor = mydb.cursor()
+    cursor.execute(CREATE_TABLE1)
+    mydb.commit()
+
+
+    CREATE_TABLE2 = '''CREATE TABLE `userdb`.`XMLTable` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+     `name` VARCHAR(500),
+     `XML` MEDIUMTEXT NULL,
+      PRIMARY KEY (`id`));'''
+
+    cursor.execute(CREATE_TABLE2)
+    mydb.commit()
+
+    xmlData = None
+
+    path_of_the_directory = 'charlie_parker'  # full directory path of musicXML files
+    items_in_folder = os.listdir(path_of_the_directory)
+    # print(items_in_folder)
+    for filename in items_in_folder:
+        f = os.path.join(path_of_the_directory, filename)
+        if os.path.isfile(f):
+            if f.endswith('.xml'):
+                with open(f, 'r') as f:
+                    xmlData = f.read()
+
+                INSERT_QUERY = f"INSERT INTO xmlTable (XML, name) VALUES (%s, %s)"
+                values = (xmlData, filename)
+                cursor.execute(INSERT_QUERY, values)
+
+                mydb.commit()
+
+
+# run from here
+createDB()
+createTables()
